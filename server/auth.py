05a2d258269b606a1631ecc7d81e40f6249117e6
ferrary7 <ary7sharma@gmail.com>
2023-07-29 16:31:52 +0530

    added environment variables

--- server/auth.py
+++ server/auth.py
@@ -9,6 +9,8 @@ from models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
+import os
+from dotenv import load_dotenv
 
 
 router = APIRouter(
@@ -16,8 +18,9 @@ router = APIRouter(
     tags=['authentication']
 )
 
-SECTRET_KEY= '00f9737fb2f4d40285e10233c707291345170b0ce156c27e6063b17e7843741e'
-ALGORITHM = 'HS256'
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM')
 
 bcrypt_context =  CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
@@ -75,11 +78,11 @@ def create_access_token(username: str, user_id:int, expires_delta: timedelta):
     encode = {'sub' : username, 'id': user_id}
     expires = datetime.utcnow() + expires_delta
     encode.update({'exp': expires})
-    return jwt.encode(encode, SECTRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try: 
-        payload = jwt.decode(token, SECTRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
         if username is None or user_id is None:
